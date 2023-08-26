7148f141f7f10bbbaf198e4753b9bd27e7f7792d
MohsenSami <mohsensami88@gmail.com>
2023-08-26 21:56:19 +0330

    props onChange input event

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@
 // import Status from './components/Status';
 // import Title from './components/Title';
 // import Wrapper from './components/Wrapper';
+// import Button from './components/Button';
 
-import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
     // const UserInfo = {
@@ -33,7 +34,8 @@ function App() {
             {/* <Wrapper>
                 <Title>Hello World</Title>
             </Wrapper> */}
-            <Button clickHandler={(e, id) => console.log(e, id)} />
+            {/* <Button clickHandler={(e, id) => console.log(e, id)} /> */}
+            <Input value="" handleChange={(e) => console.log(e.target.value)} />
         </div>
     );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,10 @@
+type InputProps = {
+    value: string;
+    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Input = ({ value, handleChange }: InputProps) => {
+    return <input type="text" value={value} onChange={handleChange} />;
+};
+
+export default Input;
